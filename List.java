afddae5b7050a84d99a1508fe072255e180b4b9b
Micaela Reyes <codemickeycode@gmail.com>
2015-03-28 00:05:42 +0800

    Added list iteration example for Java and long version of list iteration in Python

--- List.java
+++ List.java
@@ -1,1 +1,17 @@
-//3. list comprehension
+import java.util.ArrayList;
+ 
+public class ListComprehension {
+ 
+    public static void main (String[] args) {
+ 
+       ArrayList<Integer> list = new ArrayList<Integer>();
+       //for (i in Range.between(0, 10)){
+       for(int x = 0; x < 10; x++){
+          int num = x + 10;
+          list.add(num);
+       }
+ 
+       System.out.println(list.toString());
+    }
+ 
+}
--- gwtPyladies.py
+++ gwtPyladies.py
@@ -3,6 +3,15 @@ print "Hello World!"
 def add(x,y):
 	return x + y 
 
+#long version
+list = []
+for x in range(0,10):
+    x = x+10
+    list.append(x)
+print "list: "
+print list
+ 
+#short version
 list_comprehension = [x+10 for x in range(0,10)]
 print list_comprehension
 
